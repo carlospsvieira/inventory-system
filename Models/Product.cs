4402ffcb591953caa296048a200bd631b8c0c6e9
Carlos Vieira <carlepsvieira@gmail.com>
2023-07-19 19:26:52 -0300

    change dataonly att to datetime

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,16 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace inventory_system.Models
 {
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public Categories Category { get; set; }
-        public string Supplier { get; set; }
-        public int Quantity { get; set; }
-        public DateOnly EntryDate { get; set; }
-    }
+  public class Product
+  {
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public Categories Category { get; set; }
+    public string? Supplier { get; set; }
+    public int Quantity { get; set; }
+
+    [JsonIgnore]
+    public DateTime EntryDate { get; set; }
+    public string FormattedEntryDate => EntryDate.ToString("dd MMM yyyy HH:mm:ss");
+  }
 }
