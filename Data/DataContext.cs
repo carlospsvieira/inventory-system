53781344ec79f88abdd209e3cae67be38d1733e1
Carlos Vieira <carlepsvieira@gmail.com>
2023-07-28 11:26:47 -0300

    install and config entity framework and db

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace inventory_system.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Order> Orders => Set<Order>();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 global using inventory_system.Models;
 global using inventory_system.Services;
 global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+global using inventory_system.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
