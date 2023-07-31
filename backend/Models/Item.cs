3384eea305ba3ca1741d87edfff58095fd77c3d9
Carlos Vieira <carlepsvieira@gmail.com>
2023-07-31 19:31:29 -0300

    add date/time to item model

--- backend/Models/Item.cs
+++ backend/Models/Item.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace inventory_system.Models
@@ -12,5 +13,9 @@ namespace inventory_system.Models
     public string Supplier { get; set; } = string.Empty;
     public double Weight { get; set; }
     public int Quantity { get; set; }
+
+    [JsonIgnore]
+    public DateTime EntryDate { get; set; }
+    public string FormattedEntryDate => EntryDate.ToString("dd MMM yyyy HH:mm:ss");
   }
 }
