a3e780593c231138cda3668d7b4deae2b99daf35
Carlos Vieira <carlepsvieira@gmail.com>
2023-08-07 11:33:43 -0300

    add home component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <main>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'frontend';
---
+++ frontend/src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <p>
+      home works!
+    </p>
+  `,
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
